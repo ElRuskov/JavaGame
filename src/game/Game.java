4ecfe7f023c5b1dec8b5d992c9ca7afc531610e5
Joan Belusca <joan.belusca@gmail.com>
2026-02-19 11:20:54 +0100

    add Game class (TODO)

---
+++ src/game/Game.java
@@ -0,0 +1,12 @@
+package game;
+import board.Color;
+import board.Pawn;
+import board.Player;
+
+public class Game {
+    private boolean gameOver = false;
+    private Player currentPlayer;
+    private Player winner;
+    //TODO
+
+}
